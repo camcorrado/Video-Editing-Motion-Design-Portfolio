4bdf6ee3a485fe82542aba9ef403b29dadfeabf0
Cameron Corrado <camcorrado@mac-studio.mynetworksettings.com>
2024-08-01 10:44:28 -0400

    Added new reel

--- src/Components/Reels/Reels.js
+++ src/Components/Reels/Reels.js
@@ -1,14 +1,15 @@
 const reels = [
   {
     id: 0,
-    video: "https://player.vimeo.com/video/947163387",
-    alt: "Music with Miss Merry Reel",
+    video: "https://player.vimeo.com/video/993538846",
+    alt: "Animated Resume",
   },
   {
     id: 1,
-    video: "https://player.vimeo.com/video/947163315",
-    alt: "Animated Resume (2023)",
+    video: "https://player.vimeo.com/video/947163387",
+    alt: "Music with Miss Merry Reel",
   },
+
   {
     id: 2,
     video: "https://player.vimeo.com/video/947163623",
